5a25d2f2366c80e95d4da6423f7b816e2d6240c3
Harsh Mittal <mittalh863@gmail.com>
2020-08-18 01:22:33 +0530

    close invoice on click away

--- src/Components/Invoice/Invoice.tsx
+++ src/Components/Invoice/Invoice.tsx
@@ -9,6 +9,7 @@ import html2canvas from "html2canvas";
 import JSPdf from 'jspdf';
 import {MoonLoader} from "react-spinners";
 import {NotificationService} from "../../Services/notification.service";
+import {ClickAwayListener} from "@material-ui/core";
 
 export class Invoice extends Component<InvoiceProps, InvoiceState> {
     state = {
@@ -51,71 +52,73 @@ export class Invoice extends Component<InvoiceProps, InvoiceState> {
     render() {
         return (
             <Modal open={this.props.open} onClose={this.props.onClose}>
-                <div className="invoice-container" id="invoice-container">
-                    <div className="invoice-container__header">
-                        <div className="invoice-container__header__title">Invoice</div>
-                        <div className="invoice-container__header__organization-title">The Burger Builder</div>
-                        <div className="invoice-container__header__organization-address"> (+91) 1230783143 | build-my-burger.vercel.app | Ghaziabad, India</div>
-                    </div>
-                    <div className="invoice-container__customer-block">
-                        <div className="invoice-container__customer-block__name">{this.props.order.contact.name}</div>
-                        <div className="invoice-container__customer-block__customer-address">
-                            <span>{this.props.order.contact.address.street}</span>
-                            <span>Zip Code:- {this.props.order.contact.address.zip},</span>
-                            <span style={{textTransform: 'uppercase'}}>{this.props.order.contact.address.country}</span>
+                <ClickAwayListener onClickAway={this.props.onClose}>
+                    <div className="invoice-container" id="invoice-container">
+                        <div className="invoice-container__header">
+                            <div className="invoice-container__header__title">Invoice</div>
+                            <div className="invoice-container__header__organization-title">The Burger Builder</div>
+                            <div className="invoice-container__header__organization-address"> (+91) 1230783143 | build-my-burger.vercel.app | Ghaziabad, India</div>
+                        </div>
+                        <div className="invoice-container__customer-block">
+                            <div className="invoice-container__customer-block__name">{this.props.order.contact.name}</div>
+                            <div className="invoice-container__customer-block__customer-address">
+                                <span>{this.props.order.contact.address.street}</span>
+                                <span>Zip Code:- {this.props.order.contact.address.zip},</span>
+                                <span style={{textTransform: 'uppercase'}}>{this.props.order.contact.address.country}</span>
+                            </div>
+                        </div>
+                        <div className="invoice-container__invoice-details">
+                            <div className="invoice-container__invoice-details__row">
+                                <div className="invoice-container__invoice-details__row__label">Invoice No: </div>
+                                <div className="invoice-container__invoice-details__row__value"> {this.state.invoiceNo}</div>
+                            </div>
+                            <div className="invoice-container__invoice-details__row">
+                                <div className="invoice-container__invoice-details__row__label">Date Issued: </div>
+                                <div className="invoice-container__invoice-details__row__value">{moment(new Date()).format('MMMM DD, YYYY ( dddd )').toString()}</div>
+                            </div>
+                        </div>
+                        <div className="invoice-container__order-details">
+                            <div className="invoice-container__order-details__table-header">
+                                <span>Description</span>
+                                <span>Qty</span>
+                                <span>Price</span>
+                                <span>Amount</span>
+                            </div>
+                            {
+                                Object.entries(this.props.order.ingredients).map((entry) => {
+                                    return (
+                                        <div className="invoice-container__order-details__table-body-row" key={entry[0]}>
+                                            <span>{entry[0].toUpperCase()}</span>
+                                            <span>{entry[1]}</span>
+                                            <span>₹ {this.getIngredientPrice(entry[0])}</span>
+                                            <span>₹ {this.getIngredientPrice(entry[0]) * entry[1]}</span>
+                                        </div>
+                                    )
+                                })
+                            }
+                            <div className="invoice-container__order-details__table-body-row">
+                                <span>BREADS</span>
+                                <span>2</span>
+                                <span>₹ {Prices.TOP_BREAD + Prices.BOTTOM_BREAD} ({Prices.TOP_BREAD} + {Prices.BOTTOM_BREAD} )</span>
+                                <span>₹ {Prices.TOP_BREAD + Prices.BOTTOM_BREAD}</span>
+                            </div>
+                        </div>
+                        <div className="invoice-container__footer">
+                            <div className="invoice-container__footer__row">
+                                <div className="invoice-container__footer__row__label">Shipping Fee</div>
+                                <div className="invoice-container__footer__row__value">₹ {this.getShippingFee()}</div>
+                            </div>
+                            <div className="invoice-container__footer__row">
+                                <div className="invoice-container__footer__row__label">Service Tax</div>
+                                <div className="invoice-container__footer__row__value">₹ {Prices.EXTRA}</div>
+                            </div>
+                            <div className="invoice-container__footer__row">
+                                <div className="invoice-container__footer__row__label extra-bold">Total Amount</div>
+                                <div className="invoice-container__footer__row__value extra-bold">₹ {this.props.order.price}</div>
+                            </div>
                         </div>
                     </div>
-                    <div className="invoice-container__invoice-details">
-                        <div className="invoice-container__invoice-details__row">
-                            <div className="invoice-container__invoice-details__row__label">Invoice No: </div>
-                            <div className="invoice-container__invoice-details__row__value"> {this.state.invoiceNo}</div>
-                        </div>
-                        <div className="invoice-container__invoice-details__row">
-                            <div className="invoice-container__invoice-details__row__label">Date Issued: </div>
-                            <div className="invoice-container__invoice-details__row__value">{moment(new Date()).format('MMMM DD, YYYY ( dddd )').toString()}</div>
-                        </div>
-                    </div>
-                    <div className="invoice-container__order-details">
-                        <div className="invoice-container__order-details__table-header">
-                            <span>Description</span>
-                            <span>Qty</span>
-                            <span>Price</span>
-                            <span>Amount</span>
-                        </div>
-                        {
-                            Object.entries(this.props.order.ingredients).map((entry) => {
-                                return (
-                                    <div className="invoice-container__order-details__table-body-row" key={entry[0]}>
-                                        <span>{entry[0].toUpperCase()}</span>
-                                        <span>{entry[1]}</span>
-                                        <span>₹ {this.getIngredientPrice(entry[0])}</span>
-                                        <span>₹ {this.getIngredientPrice(entry[0]) * entry[1]}</span>
-                                    </div>
-                                )
-                            })
-                        }
-                        <div className="invoice-container__order-details__table-body-row">
-                            <span>BREADS</span>
-                            <span>2</span>
-                            <span>₹ {Prices.TOP_BREAD + Prices.BOTTOM_BREAD} ({Prices.TOP_BREAD} + {Prices.BOTTOM_BREAD} )</span>
-                            <span>₹ {Prices.TOP_BREAD + Prices.BOTTOM_BREAD}</span>
-                        </div>
-                    </div>
-                    <div className="invoice-container__footer">
-                        <div className="invoice-container__footer__row">
-                            <div className="invoice-container__footer__row__label">Shipping Fee</div>
-                            <div className="invoice-container__footer__row__value">₹ {this.getShippingFee()}</div>
-                        </div>
-                        <div className="invoice-container__footer__row">
-                            <div className="invoice-container__footer__row__label">Service Tax</div>
-                            <div className="invoice-container__footer__row__value">₹ {Prices.EXTRA}</div>
-                        </div>
-                        <div className="invoice-container__footer__row">
-                            <div className="invoice-container__footer__row__label extra-bold">Total Amount</div>
-                            <div className="invoice-container__footer__row__value extra-bold">₹ {this.props.order.price}</div>
-                        </div>
-                    </div>
-                </div>
+                </ClickAwayListener>
                 <div className="download-button">
                     {
                         !this.state.loading ?
